bcae968e0ab21a1d2eb39def81b8526cdc1e808f
losrobbos <50549469+losrobbos@users.noreply.github.com>
2021-04-29 08:43:54 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,24 +10,6 @@ app.use( express.json() ) // allow sending JSON data to us...
 
 
 
-/** API PROTOTYPE
- * 
- * In an API prototype all routes return hardcoded data
- * 
- * A prototype is a simplified API which serves the data needed to the frontend, 
- * but with a very slim, minimal structure: No database, no controllers, no JWT, no middleware
- * 
- * The prototype has ONE central goal: 
- * Clarify the DATA EXCHANGE between frontend and backend
- * And therefore allow EARLY INTEGRATION of frontend & backend to test if the flow of information
- * works as expected. That is the most critical hump in realizing a fullstack app.
- * 
- * Once the data structure to exchange is clarified,we can setup schemas & models 
- * and replace our hardcoded data in all our routes with actual database calls 
- * 
- * HAPPY PROTOTYPING!
-*/
-
 // HOME ROUTE
 app.get('/', (req, res) => {
   res.send('Hello World!');
